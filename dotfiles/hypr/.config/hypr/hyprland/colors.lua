74e640ea28a0b69c25214465609c2f36d35288e0
Jhostin-dev <jhostinjosue23@gmail.com>
2026-07-26 14:59:16 -0500

    feat: initial Linux_Personalizacion setup

---
+++ dotfiles/hypr/.config/hypr/hyprland/colors.lua
@@ -0,0 +1,16 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(48474377)",
+            inactive_border = "rgba(1c1b1933)",
+        },
+    },
+    misc = {
+        background_color = "rgba(141311FF)",
+    },
+})
+
+hl.window_rule({
+    match        = { pin = 1 },
+    border_color = "rgba(ccc7acAA) rgba(ccc7ac77)",
+})
